6066084f26ae7eb695e5f26b3248d660b2e84b2d
gneidhardt <84924248+gneidhardt@users.noreply.github.com>
2023-07-25 16:05:36 -0500

    Update 2_check_urls_a-z_list.py
    Added time.sleep function

--- 2_check_urls_a-z_list.py
+++ 2_check_urls_a-z_list.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import time
 
 # clean-up
 try:
@@ -8,23 +9,24 @@ except FileNotFoundError:
     pass
 
 # MAIN
+# take out def in jupyter notebook and move everything over
 def main():
     with open('urls.txt', 'r', newline="", encoding="utf-8") as txtfile:
         lines = txtfile.read().splitlines()
-        
         for line in lines:
             split = line.split('\t')
             title = split[0]
-            link = split[0]
-
+            url = split[0]
+            
             # Loop through links checking for 404 responses, and append to list.
-            _validate_url(title, link)
+            _validate_url(title, url)
 
 
 # Internal function for validating HTTP status code.
+# take out def in jupyter notebook and move everything over
 def _validate_url(title, url):
     note = ""
-
+    
     try:
         r = requests.head(url)
     except Exception as e:
@@ -40,12 +42,14 @@ def _validate_url(title, url):
         status_code = r.status_code
     except Exception as e:
         status_code = ""
-        note = "No response"
-
+        note = "No status code"
+    
     # write to file
     with open('log.txt', 'a', encoding="utf-8") as writer:
         writer.write(f"{title}\t{url}\t{status_code}\t{redirect_location}\t{note}\n")
-
-
+    
+    time.sleep(10)
+    
+# maybe take this out if providing an error    
 if __name__ == "__main__":
     main()
